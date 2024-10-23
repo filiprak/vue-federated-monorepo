3580304d6e2368af94d35221b8816b8dfc9cdf33
Filip Rak <filip.rak@ikol.com>
2024-10-23 12:21:13 +0200

    Cleanup

--- packages/webapp/src/index.ts
+++ packages/webapp/src/index.ts
@@ -2,8 +2,18 @@ import { random } from '@/utils/index';
 import { button } from '@/ui/index';
 import { ref } from 'vue';
 
+import { __federation_method_ensure } from '__federation__';
+
+const importModule = async (path: string) => {
+    return __federation_method_ensure('@/')
+        .then((m: Record<string, Function>) => m.get(`./${path.slice(2)}`))
+        .then((m: Function) => m());
+};
+
 const r = ref(7);
 
 console.log('webapp entry', random());
 console.log('webapp entry', button());
 console.log('webapp entry', r);
+
+window.importModule = importModule;
