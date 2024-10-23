2218714f859fddf47432f63459fc3d64f624b72f
Filip Rak <filip.rak@ikol.com>
2024-10-23 14:11:39 +0200

    Cleanup [skip ci]

--- packages/webapp/src/index.ts
+++ packages/webapp/src/index.ts
@@ -2,13 +2,7 @@ import { random } from '@/utils/index';
 import { button } from '@/ui/index';
 import { ref } from 'vue';
 
-import { __federation_method_ensure } from '__federation__';
-
-const importModule = async (path: string) => {
-    return __federation_method_ensure('@/')
-        .then((m: Record<string, Function>) => m.get(`./${path.slice(2)}`))
-        .then((m: Function) => m());
-};
+import * as f from '__federation_fn_import';
 
 const r = ref(7);
 
@@ -16,4 +10,4 @@ console.log('webapp entry', random());
 console.log('webapp entry', button());
 console.log('webapp entry', r);
 
-window.importModule = importModule;
+window.importShared = f.importShared;
