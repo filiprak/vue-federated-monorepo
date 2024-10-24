aa225e0c7436199b7b7dfa33c2eae3b850b9ec68
Filip Rak <filip.rak@ikol.com>
2024-10-25 00:48:15 +0200

    Cleanup

---
+++ packages/webapp/src/bootstrap.ts
@@ -0,0 +1,15 @@
+import { ref } from 'vue';
+import { random } from '@/utils/index';
+import { button } from '@/ui/components/Button';
+import { button as b2 } from '@/ui/index';
+// import { remote } from 'xyz/index';
+// import * as f from '__federation__';
+
+const r = ref(7);
+
+console.log('webapp entry', random());
+console.log('webapp entry', button(), b2());
+console.log('webapp entry', r);
+// console.log('webapp entry', remote());
+
+// window.f = f;
